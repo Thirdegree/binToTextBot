b5b643d93515d5cd32fce5a11bc998313742b156
Joel Christiansen <greenbadge.jc@gmail.com>
2014-03-26 23:46:37 -0600

    working

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import praw
 from collections import deque
 from time import sleep
+import re
 
 
 r = praw.Reddit("binaryToTextBot by /r/thirdegree")
@@ -16,7 +17,7 @@ done = deque(maxlen=200)
 Trying = True
 while Trying:
 	try:
-		#USERNAME = _login()
+		USERNAME = _login()
 		Trying = False
 	except praw.errors.InvalidUserPass:
 		print "Invalid Username/password, please try again."
@@ -24,25 +25,33 @@ while Trying:
 def main():
 	comments = praw.helpers.comment_stream(r,'all', limit = 1000)
 	for i in comments:
- 		translate = ""
 		if i.id in done:
 			continue
 		done.append(i.id)
-		for j in i.body.split():
-			j = j.strip()
-			if len(j)%8 == 0 and all([k in ["0","1"] for k in j]):
-				translate += "> " + j + "\n\n"
-				translate += ''.join(chr(int(j[l:l+8], 2)) for l in xrange(0, len(j), 8)) + "\n\n"
-		if translate != "":
-			print translate
-			#i.reply(translate)
+		translated = translate(i.body)
+		if translated != "":
+			i.reply(translated)
 			sleep(2)
+
+def translate(body):
+	pattern = r"([10 ]+)+"
+	matches = re.findall(pattern, body)
+	translated = ""
+	if all([i == ' ' for i in matches]):
+		return ""
+	for word in matches:
+		word_fixed = re.sub(" ", "", word)
+		if len(word_fixed) == 0:
+			continue
+		if len(word_fixed)%8 == 0 and all([k in ["0","1"] for k in word_fixed]):
+			translated += ("> "+ word+"\n\n")
+			translated += (''.join(chr(int(word_fixed[l:l+8], 2)) for l in xrange(0, len(word_fixed), 8)) + "\n\n")
+	return "".join(translated)
 
 
 
 try: 
-	print "tick"
-	sleep(5)
+	pass
 	main()
 except Exception as e:
 	print e
